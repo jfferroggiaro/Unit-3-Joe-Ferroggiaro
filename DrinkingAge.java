d5ef5ff2dc86dfda472a19a920af580c909a8f68
Joe Ferroggiaro <ferroggj@wlhs.wlwv.k12.or.us>
2019-10-03 09:23:26 -0700

    drinking age

---
+++ DrinkingAge.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DrinkingAge{
+  public static void main(String[] args){
+    Scanner input = new Scanner();
+    System.out.println("Enter Your Age");
+    int age = input.nextInt();
+    if(age >= 21){
+      System.out.println("You may drink in this country.");
+    }else{
+      System.out.println("You may not drink in this country.");
+    }
+  }
+}
