71eb66a8612a5b133afcb6374394ca2ff3a7fd81
Joe Ferroggiaro <jfferroggiaro@gmail.com>
2019-10-04 08:46:03 -0700

    finish temptest

--- TempTest.java
+++ TempTest.java
@@ -5,6 +5,14 @@ public class TempTest{
     Scanner input = new Scanner(System.in);
     System.out.println("What is the temperature?");
     int temp = input.nextInt();
-    
+    if(temp < 45){
+      System.out.println("cold");
+    } else if(temp < 68){
+      System.out.println("cool");
+    } else if(temp < 84){
+      System.out.println("ideal");
+    } else {
+      System.out.println("hot");
+    }
   }
 }
