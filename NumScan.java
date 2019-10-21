50cc6ac266e6228daabebcb7161d5488b45c6126
Joe Ferroggiaro <jfferroggiaro@gmail.com>
2019-10-21 09:23:59 -0700

    MiniProject

--- NumScan.java
+++ NumScan.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class NumScan{
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
-    System.out.println("NumScan \n Please enter an integer...");
+    System.out.println("    NumScan\nPlease enter an integer...");
+    int input = scan.nextInt();
+    int zeroCount = 0;
+    int oddCount = 0;
+    int evenCount = 0;
+    for(int i = 1; 1)
   }
 }
